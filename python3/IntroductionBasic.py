2908fe38f57667909fc60499c33b140044753d30
earljadecara <earljade@zyyx.jp>
2017-07-29 14:47:01 +0800

    Tutorial till Function Parameters Defaults

--- python3/IntroductionBasic.py
+++ python3/IntroductionBasic.py
@@ -4,6 +4,7 @@ Garage = "Ferrari", "Honda", "Porsche", "Toyota"
 for each_car in Garage:
     print(each_car)
 
+print '\n'
 # Print function and strings
 #   print
 print('Single Quotes')
@@ -15,6 +16,7 @@ print 'can do this for float', 0.13
 print('you\'ll have success here')
 print("you'll have success here too")
 
+print '\n'
 # Math
 #   Addition
 print 1+3
@@ -27,12 +29,14 @@ print 1/3
 #   Division(float)
 print 1/3.0
 
+print '\n'
 # Variables
 var1 = 13
 var2 = 31
 var3 = 'Hey!'
 print var3, (var1 + var2)
 
+print '\n'
 # While loop
 condition = 1
 print '--Start loop--'
@@ -41,6 +45,7 @@ while condition < 10:
     condition += 1
 print '--End loop--'
 
+print '\n'
 # For loop
 exampleList = [1, 5, 6, 6, 2, 1, 5, 2, 1, 4]
 
@@ -52,3 +57,24 @@ print '--Start loop--'
 for x in range(1, 11):
     print(x)
 print '--End loop--'
+
+print '\n'
+# If...Else
+x = 5
+y = 8
+if x > 55:
+    print('x is greater than 55')
+else:
+    print('x is not greater than 55')
+
+print '\n'
+# If...Elif...Else
+x = 5
+y = 10
+z = 22
+if x > y:
+    print('x is greater than y')
+elif x < z:
+    print('x is less than z')
+else:
+    print('if and elif never ran...')
---
+++ python3/IntroductionFunction.py
@@ -0,0 +1,24 @@
+# Function
+#   Define function
+def example():
+    print('this code will run')
+    z = 4 + 9
+    print(z)
+#   Call function
+example()
+
+print '\n'
+# Function Parameters
+def simple_addition(num1, num2):
+    answer = num1 + num2
+    print 'num1 is', num1
+    print(answer)
+simple_addition(5, 8)
+
+print '\n'
+# Function Parameters Defaults
+def basic_window(width,height,font='TNR'):
+    # let us just print out everything
+    print(width, height, font)
+basic_window(350, 500)
+basic_window(350, 500, font='courier')
